a4317ac32c6a88ff6c9b3a6d55086514ac4c79d1
Ashleehernandez <149027004+Ashleehernandez@users.noreply.github.com>
2025-07-31 16:50:10 -0400

    commit

---
+++ StudentManagerApplication.Intraestructura/IoC/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StudentManagerApplication.Applicaction.IRepository;
+using StudentManagerApplication.Applicaction.IService;
+using StudentManagerApplication.Applicaction.Service;
+using StudentManagerApplication.Intraestructura.Data;
+using StudentManagerApplication.Intraestructura.Repository;
+
+namespace StudentManagerApplication.Intraestructura.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register the DbContext with SQL Server
+            services.AddDbContext<ApplicationDbContextDB>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Register the repository and service
+            services.AddScoped<IRepositoryStudents, RepositoryStudents>();
+            services.AddScoped<IServiceStudents, ServiceStudents>();
+            return services;
+        }
+    }
+}
