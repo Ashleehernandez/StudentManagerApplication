d8f5844680eae4904047fbf7c412f0de90810c1e
Ashleehernandez <149027004+Ashleehernandez@users.noreply.github.com>
2025-07-31 16:17:46 -0400

    commit

---
+++ StudentManagerApplication.Applicaction/Service/ServiceStudents.cs
@@ -0,0 +1,38 @@
+﻿using StudentManagerApplication.Applicaction.IService;
+using StudentManagerApplication.Domain.Entity;
+
+namespace StudentManagerApplication.Applicaction.Service
+{
+    public class ServiceStudents : IServiceStudents
+    {
+        public Task AddStudentAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteStudentAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Student>> GetAllStudentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetStudentByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> StudentExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStudentAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
