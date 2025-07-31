545dd18a521feb1f80e2341fd400fb2f6bda6ae4
Ashleehernandez <149027004+Ashleehernandez@users.noreply.github.com>
2025-07-31 16:02:54 -0400

    commit

---
+++ StudentManagerApplication.Intraestructura/Data/ApplicationDbContextDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagerApplication.Domain.Entity;
+
+namespace StudentManagerApplication.Intraestructura.Data
+{
+    public class ApplicationDbContextDB : DbContext
+    {
+        public ApplicationDbContextDB(DbContextOptions<ApplicationDbContextDB> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Students");
+            modelBuilder.Entity<Student>().HasKey(s => s.Id);
+            modelBuilder.Entity<Student>().Property(s => s.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Student>().Property(s => s.LastName).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Student>().Property(s => s.Email).IsRequired().HasMaxLength(200);
+            modelBuilder.Entity<Student>().Property(s => s.PhoneNumber).HasMaxLength(15);
+            modelBuilder.Entity<Student>().Property(s => s.Address).HasMaxLength(250);
+            modelBuilder.Entity<Student>().Property(s => s.City).HasMaxLength(100);
+            modelBuilder.Entity<Student>().Property(s => s.State).HasMaxLength(100);
+            modelBuilder.Entity<Student>().Property(s => s.ZipCode).HasMaxLength(20);
+        }
+    }
+}
